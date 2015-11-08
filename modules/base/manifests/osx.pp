0331a7569b014ce037412e2208296ea37ce2d0da
marano <thiagomarano@gmail.com>
2015-11-08 19:52:39 -0200

    Setup macdefaults.

--- modules/base/manifests/osx.pp
+++ modules/base/manifests/osx.pp
@@ -1,16 +1,18 @@
 class base::osx {
-  include osx
+  include macdefaults
+  include osx::dock::autohide
+  include osx::finder::show_external_hard_drives_on_desktop
+  include osx::finder::show_hidden_files
+  include osx::finder::show_mounted_servers_on_desktop
+  include osx::finder::show_removable_media_on_desktop
+  include osx::finder::unhide_library
+  include osx::global::enable_standard_function_keys
+  include osx::keyboard::capslock_to_control
+  include osx::no_network_dsstores
+  include osx::safari::enable_developer_mode
+  include osx::software_update
 
   class { 'boxen::security':
     require_password => false
   }
-
-  osx::dock::autohide
-  osx::finder::show_external_hard_drives_on_desktop
-  osx::finder::show_hidden_files
-  osx::finder::show_mounted_servers_on_desktop
-  osx::finder::show_removable_media_on_desktop
-  osx::finder::unhide_library
-  osx::global::enable_standard_function_keys
-  osx::software_update
 }
