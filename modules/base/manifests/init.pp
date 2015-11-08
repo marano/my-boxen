77554e3705315622f05d0584ade5f724753d8cca
marano <thiagomarano@gmail.com>
2015-11-08 19:21:40 -0200

    Alphabetical order.

--- modules/base/manifests/init.pp
+++ modules/base/manifests/init.pp
@@ -34,8 +34,8 @@ class base {
     ]: provider => 'brewcask'
   }
 
-  appstore::app { 'Slack':          source => 'slack/id803453959' }
   appstore::app { 'Evernote':       source => 'evernote/id406056744' }
-  appstore::app { 'The Unarchiver': source => 'the-unarchiver/id425424353' }
   appstore::app { 'Shush':          source => 'shush-microphone-manager/id496437906' }
+  appstore::app { 'Slack':          source => 'slack/id803453959' }
+  appstore::app { 'The Unarchiver': source => 'the-unarchiver/id425424353' }
 }
