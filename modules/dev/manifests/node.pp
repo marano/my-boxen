1628015563bf7122612e0356ae491d2c973c2067
marano <thiagomarano@gmail.com>
2015-11-09 21:53:30 -0200

    Only installs node 5 since latest 4 doesnt seem to be available.

--- modules/dev/manifests/node.pp
+++ modules/dev/manifests/node.pp
@@ -1,11 +1,10 @@
 class dev::node {
   include nodejs
 
-  nodejs::version { '4.2.2': }
   nodejs::version { '5.0.0': }
 
   class { 'nodejs::global':
-    version => '4.2.2'
+    version => '5.0.0'
   }
 
   npm_module { 'bower for all nodes':
