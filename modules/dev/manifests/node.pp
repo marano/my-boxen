8fddf311861ad331f7dea427c3b47b60bb3d9573
marano <thiagomarano@gmail.com>
2015-11-09 20:55:45 -0200

    Install LTS node and define it as global default. Also installs bower for every node version.

--- modules/dev/manifests/node.pp
+++ modules/dev/manifests/node.pp
@@ -1,5 +1,15 @@
 class dev::node {
   include nodejs
 
+  nodejs::version { '4.2.2': }
   nodejs::version { '5.0.0': }
+
+  class { 'nodejs::global':
+    version => '4.2.2'
+  }
+
+  npm_module { 'bower for all nodes':
+    module       => 'bower',
+    node_version =>  '*'
+  }
 }
