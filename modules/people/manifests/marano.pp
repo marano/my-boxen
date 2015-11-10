1ca97cf7d4b6abd7edb6236d196861429e395081
marano <thiagomarano@gmail.com>
2015-11-10 01:19:34 -0200

    Get rid of dotfiles module.

--- modules/people/manifests/marano.pp
+++ modules/people/manifests/marano.pp
@@ -2,8 +2,19 @@ class people::marano {
   include art
   include essentials
   include dev
-  include dotfiles
   include gamer
 
   include iterm2::colors::solarized_dark
+
+  $dotfiles       = "${boxen::config::srcdir}/setup.bat"
+  $setup_dotfiles = "${boxen::config::srcdir}/setup.bat/link_dotfiles"
+
+  repository { $dotfiles:
+    source  => 'marano/setup.bat',
+    require => File[$boxen::config::srcdir]
+  }
+
+  exec { $setup_dotfiles:
+    require => Repository[$dotfiles]
+  }
 }
