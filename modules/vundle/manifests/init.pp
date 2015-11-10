f017720afe10b03fadbd29dde49a4d712d9e468a
marano <thiagomarano@gmail.com>
2015-11-09 22:25:38 -0200

    Do not create a ~/.rbenv folder.

--- modules/vundle/manifests/init.pp
+++ modules/vundle/manifests/init.pp
@@ -28,7 +28,7 @@ class vundle {
     ]
   }
 
-  exec { 'rbenv local system && rbenv rehash && bundle && rake make':
+  exec { 'rbenv local system && gem install rake && rake make':
     cwd     => "${user_home}/.vim/bundle/command-t/",
     onlyif  => 'file -d ~/.vim/bundle/command-t',
     require => Exec[$vundle_install]
