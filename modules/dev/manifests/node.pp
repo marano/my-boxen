b65c30d40b8b534a4b487f1e7202cf1740863db1
marano <thiagomarano@gmail.com>
2015-12-05 11:41:44 +0700

    Install node through nvm and brew.

--- modules/dev/manifests/node.pp
+++ modules/dev/manifests/node.pp
@@ -1,14 +1,7 @@
 class dev::node {
-  include nodejs
-
-  nodejs::version { '5.0.0': }
-
-  class { 'nodejs::global':
-    version => '5.0.0'
-  }
-
-  npm_module { 'bower for all nodes':
-    module       => 'bower',
-    node_version =>  '*'
+  package {
+    [
+      'nvm'
+    ]:
   }
 }
