dcab9f182c1b9a38f0ec6d77b8a1f8ab1bc6c449
marano <thiagomarano@gmail.com>
2015-12-05 17:09:10 +0700

    Install nvm by cloning it from github.

--- modules/dev/manifests/node.pp
+++ modules/dev/manifests/node.pp
@@ -1,7 +1,13 @@
 class dev::node {
-  package {
-    [
-      'nvm'
-    ]:
+  $nvm_folder = "/Users/${::boxen_user}/.nvm"
+
+  repository { $nvm_folder:
+    source => 'creationix/nvm',
+    notify => Exec['checkout latest nvm version']
+  }
+
+  exec { 'checkout latest nvm version':
+    command => 'git checkout `git describe --abbrev=0 --tags`',
+    cwd     => $nvm_folder
   }
 }
